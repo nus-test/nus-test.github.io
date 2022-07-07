9e9b8cfc7d1a002506fc395943137aab87cf8a65
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2022-07-07 17:30:17 +0800

    Add bugs from CockroachDB and TiDB

--- export.py
+++ export.py
@@ -16,11 +16,17 @@ domain_map = {
 }
 
 
-for (domain,) in c.execute('SELECT DISTINCT domain FROM bugs;').fetchall():
-	print('<h2>%s</h2>' % (domain_map[domain], ))
-	for title, url, found_by in c.execute("SELECT title, url, reported_by FROM bugs WHERE domain='%s';" % (domain)).fetchall():
-		print('<details>')
-		print('<summary>%s</summary>'% (title, ))
-		print('link: <a href="%s">%s</a> <br />' % (url, url))
-		print('found by: %s' % (found_by,))
-		print('</details>')
+common_where_clause = "(resolution IN ('confirmed', 'fixed', 'open') OR resolution IS NULL)"
+for domain, count in c.execute('SELECT domain, COUNT(*) FROM bugs WHERE ' + common_where_clause + ' GROUP BY domain;').fetchall():
+    print('<h2>%s (%d bugs)</h2>' % (domain_map[domain], count))
+    for system, count in c.execute(("SELECT system, COUNT(*) FROM bugs WHERE domain = '%s' AND " + common_where_clause + " GROUP BY system") % (domain,)).fetchall():
+        print('<h3>%s (%d bugs)</h3>' % (system, count))
+        for title, url, found_by, resolution in c.execute(("SELECT title, url, reported_by, resolution FROM bugs WHERE domain='%s' AND system='%s' AND " + common_where_clause + ";") % (domain, system)).fetchall():
+            print('<details>')
+            print('<summary>%s</summary>'% (title, ))
+            if resolution is None:
+                resolution = 'unconfirmed'
+            print('Status: %s<br />' % (resolution, ))
+            print('Link: <a href="%s">%s</a> <br />' % (url, url))
+            print('Found by: %s' % (found_by,))
+            print('</details>')
--- update.py
+++ update.py
@@ -1,18 +1,28 @@
 import json
 import re
+import os
 from github import Github
 
 
 f = open("data/bugs.json", "r")
 original_content = f.read()
 parsed_content = json.loads(original_content)
-g = Github()
+access_token = os.environ.get('GITHUB_LAB_TOKEN', None)
+g = Github(access_token)
 
 def format_date(date):
     return date.strftime("%d/%m/%Y")
 
 repo_to_domain_mapping = {
-    'duckdb' : 'dbms'
+    'duckdb' : 'dbms',
+    'cockroach' : 'dbms',
+    'tidb' : 'dbms'
+}
+
+repo_to_system_mapping = {
+    'duckdb' : 'DuckDB',
+    'cockroach' : 'CockroachDB',
+    'tidb' : 'TiDB'
 }
 
 for entry in parsed_content:
@@ -29,13 +39,15 @@ for entry in parsed_content:
                 entry['title'] = issue.title
             if 'created_at' not in entry:
                 entry['created_at'] = format_date(issue.created_at)
-            if 'closed_at' not in entry:
-                entry['fix_date'] = format_date(issue.closed_at)
-            if 'state' not in entry:
+            if 'closed_at' not in entry and issue.closed_at:
+                entry['closed_at'] = format_date(issue.closed_at)
+            if issue.state: # always update the state
                 entry['state'] = issue.state
             if 'domain' not in entry:
                 entry['domain'] = repo_to_domain_mapping[repo_name]
             if 'reported_by' not in entry:
                 entry['reported_by'] = issue.user.login
+            if 'system' not in entry:
+                entry['system'] = repo_to_system_mapping[repo_name]
 
 print(json.dumps(parsed_content, indent=4))
